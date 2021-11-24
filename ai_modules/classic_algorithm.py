68cf907ce7b986c924cc2ac016e0a7b8f4be1d79
Jarkynbyek Japar <jarkynbyekjapar@Jarkynbyeks-MacBook-Pro.local>
2021-11-24 19:41:20 +0900

    just commit

--- ai_modules/classic_algorithm.py
+++ ai_modules/classic_algorithm.py
@@ -22,10 +22,10 @@ class MinimaxAgent():
             The evaluation or utility and the action index
         """
         list_action = AIElements.get_possible_action(state)
-        eval_score, selected_action_index = self._minimax(0,state,True)
+        eval_score, selected_action_index = self._minimax(0,state,True,float('-inf'),float('inf'))
         return (eval_score,list_action[selected_action_index])
     
-    def _minimax(self, current_depth, state, is_max_turn):
+    def _minimax(self, current_depth, state, is_max_turn, alpha, beta):
 
         if current_depth == self.max_depth or state.is_terminal():
             return AIElements.evaluation_function(state, self.player_color), None
@@ -39,14 +39,20 @@ class MinimaxAgent():
         for action_index in index_of_actions:
             new_state = AIElements.result_function(state,possible_action[action_index])
 
-            eval_child, action_child = self._minimax(current_depth+1,new_state,not is_max_turn)
+            eval_child, action_child = self._minimax(current_depth+1,new_state,not is_max_turn, alpha, beta)
 
             if is_max_turn and best_value < eval_child:
                 best_value = eval_child
                 action_target = action_index
+                alpha = max(alpha, best_value)
+                if beta <= alpha:
+                    break
 
             elif (not is_max_turn) and best_value > eval_child:
                 best_value = eval_child
                 action_target = action_index
+                beta = min(beta, best_value)
+                if beta <= alpha:
+                    break
 
         return best_value, action_target
